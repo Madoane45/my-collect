104a00640af993d08a7ebd81f4ee8d39d669326a
Madoane45 <Madoane45@gmail.com>
2021-02-09 13:46:43 -0500

    commit tree abecdda23f699810cf5003119dc4277f6d573026

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
-
+require "pry"
+def my_collect(array)
+ counter = 0 #index
+  new_array = []
+  while counter < array.length
+    new_array << yield(array[counter])
+    counter += 1
+    end
+  new_array
+end
+binding.pry 
